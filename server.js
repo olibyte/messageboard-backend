4a82eb0183a1d5a902383792a95586908bdc0902
Oliver <ocben1@gmail.com>
2020-04-18 13:03:24 +1000

    express set up

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello express!');
+var messages = [{text: 'some text', owner: 'Tim'}, {text: 'another text', owner: 'Jane'}];
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
 })
 
 app.listen(1234);
